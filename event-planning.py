7ccfa014e6dd8ec488a3fbe49f921cba24fb327f
Hardeep <hardeep.chana@gmail.com>
2024-04-16 00:11:07 +0100

    added event planning script and renamed first script to kebab case (#1)
    * added event planning script and renamed first script to kebab case
    
    * updated locations

---
+++ event-planning.py
@@ -0,0 +1,14 @@
+# Task 1: Defining the initial variables
+event_date = "September 12 2024"
+location = "Barton Football Club"
+confirm_guests = 25
+
+# Task 2: Update and Display Event Details
+# New location and additional RSVPs
+location = "Barton Village hall"
+additional_rsvps = 5
+confirm_guests += additional_rsvps # Updated guest count
+
+# Print the updated details
+print("Updated event location:", location)
+print("Updated guest count:", confirm_guests)
